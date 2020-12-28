8408f7d7c89a7c26f1a5dfc37bb903641376f901
Robin P Joseph <robin.r.joseph@ericsson.com>
2020-12-28 15:20:40 +0100

    add links

--- src/components/swipeview.js
+++ src/components/swipeview.js
@@ -23,13 +23,24 @@ export default class SwipeView extends React.Component{
        return(
         <SwipeableViews>
         <div style={Object.assign({}, styles.slide, styles.slide1)}>
-          slide n°1
+        Hat1: Coder
+        <br/><br/>
+        <a href="https://github.com/16pj" target="_blank" rel="noreferrer">Github Playground</a>
         </div>
-        <div style={Object.assign({}, styles.slide, styles.slide2)}>
-          slide n°2
+        <div style={Object.assign({}, styles.slide, styles.slide1)}>
+        Hat2: Decoder
+        <br/><br/>
+        <a href="https://www.linkedin.com/in/robinpj" target="_blank" rel="noreferrer">Linkedin Profile</a>
         </div>
-        <div style={Object.assign({}, styles.slide, styles.slide3)}>
-          slide n°3
+        <div style={Object.assign({}, styles.slide, styles.slide1)}>
+        Hat3: Learner
+        <br/><br/>
+        <a href="http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8241091&isnumber=8241062" target="_blank" rel="noreferrer">IEEE Paper</a>
+        </div>
+        <div style={Object.assign({}, styles.slide, styles.slide1)}>
+        Hat4: Doer
+        <br/><br/>
+        <a href="https://www.youracclaim.com/badges/1dbdfb09-bb95-463e-b2cc-191ee3d1a412/linked_in_profile" target="_blank" rel="noreferrer">Ericsson Recognition</a>
         </div>
       </SwipeableViews>
        )
